64fe730863e0e6a0476901160a761f8afff57f3b
JuanTabares1 <juanjosetabarestrivino@gmail.com>
2024-10-04 17:29:30 -0500

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+    const scrollDownBtn = document.querySelector('.scroll-down a');
+    const sections = document.querySelectorAll('section');
+
+    scrollDownBtn.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevenir la acción predeterminada del enlace
+
+        // Encontrar la sección activa
+        const currentSectionIndex = [...sections].findIndex(section => {
+            const rect = section.getBoundingClientRect();
+            return rect.top >= 0 && rect.top < window.innerHeight;
+        });
+
+        // Desplazarse a la siguiente sección si hay una
+        if (currentSectionIndex !== -1 && currentSectionIndex < sections.length - 1) {
+            sections[currentSectionIndex + 1].scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+
