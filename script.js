01d5ce9f499b5ec37fdd9c82d771bfe4d784a963
JuanTabares1 <juanjosetabarestrivino@gmail.com>
2024-10-16 14:55:17 -0500

    commit

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+const scrollDownBtn = document.querySelector('.scroll-down a');
+const sections = document.querySelectorAll('section');
 
-    const scrollDownBtn = document.querySelector('.scroll-down a');
-    const sections = document.querySelectorAll('section');
-
-    scrollDownBtn.addEventListener('click', (e) => {
-        e.preventDefault(); // Prevenir la acción predeterminada del enlace
+scrollDownBtn.addEventListener('click', (e) => {
+    e.preventDefault(); // Prevenir la acción predeterminada del enlace
 
         // Encontrar la sección activa
-        const currentSectionIndex = [...sections].findIndex(section => {
-            const rect = section.getBoundingClientRect();
-            return rect.top >= 0 && rect.top < window.innerHeight;
-        });
-
-        // Desplazarse a la siguiente sección si hay una
-        if (currentSectionIndex !== -1 && currentSectionIndex < sections.length - 1) {
-            sections[currentSectionIndex + 1].scrollIntoView({ behavior: 'smooth' });
-        }
+    const currentSectionIndex = [...sections].findIndex(section => {
+        const rect = section.getBoundingClientRect();
+        return rect.top >= 0 && rect.top < window.innerHeight;
     });
 
+    // Desplazarse a la siguiente sección si hay una
+    if (currentSectionIndex !== -1 && currentSectionIndex < sections.length - 1) {
+        sections[currentSectionIndex + 1].scrollIntoView({ behavior: 'smooth' });
+    }
+});
